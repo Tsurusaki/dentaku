319fe2cc5dad0011b061f55f8b1f8bc2b898ef50
鶴崎かんな <KANNA@tsurusakikanna-no-MacBook-Air.local>
2015-02-22 16:38:13 +0900

    first commit

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -9,6 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int number;
+    int number2;
+    int number3;
+    IBOutlet UILabel *label;
+    IBOutlet UILabel *label2;
+    IBOutlet UILabel *label3;
+    int ope;
+    
+}
+-(IBAction)bt1;
+-(IBAction)bt2;
+-(IBAction)bt3;
+-(IBAction)bt4;
+-(IBAction)bt5;
+-(IBAction)bt6;
+-(IBAction)bt7;
+-(IBAction)bt8;
+-(IBAction)bt9;
+-(IBAction)bt0;
+-(IBAction)ikoru;
+
+
+
 
 
 @end
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -14,6 +14,75 @@
 
 @implementation ViewController
 
+-(IBAction)bt1{
+    if(ope==0){
+    number=number*10+1;
+    label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+1;
+        label2.text=[NSString stringWithFormat:@"%d",number2];
+        }
+}
+
+-(IBAction)bt2{
+    number=number*10+2;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt3{
+    number=number*10+3;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt4{
+    number=number*10+4;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt5{
+    number=number*10+5;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt6{
+    number=number*10+6;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt7{
+    number=number*10+7;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt8{
+    number=number*10+8;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt9{
+    number=number*10+9;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)bt0{
+    number=number*10;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
+
+-(IBAction)purasu{
+    
+    ope=1;
+}
+
+-(IBAction)ikoru{
+    number3=number+number2;
+    label3.text=[NSString stringWithFormat:@"%d",number];
+}
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
